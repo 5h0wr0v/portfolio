1c4b613975c5b84362897e37e931590cccaaddfa
5h0wr0v <azam15-5843@diu.edu.bd>
2026-09-13 01:19:04 +0600

    Optimize font loading to eliminate lint warnings and ensure clean Next.js build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,14 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${dmSans.variable} ${robotoSlab.variable} h-full scroll-smooth antialiased`}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className={`${dmSans.className} min-h-full bg-white text-slate-800`}>
         {children}
       </body>
